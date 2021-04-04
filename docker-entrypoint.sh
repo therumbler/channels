56af0fa973419210176358e88b70ed1397d4fbc7
Benjamin Rumble <therumbler@gmail.com>
2021-04-04 14:56:59 -0400

    got ffmpeg audio working

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,27 +4,13 @@ rm /live/*.ts
 rm /live/*.m3u8
 
 ffmpeg  \
-    -i http://10.0.1.238:5004/auto/v9.1?transcode=mobile \
-    -r 15 \
-    -codec copy \
-    -copyts \
-    -copytb 1 \
+    -i http://10.0.1.238:5004/auto/v29.1?transcode=mobile \
+    -acodec aac \
+    -ac 2 \
+    -vcodec libx264 \
     -map 0 \
     -f hls \
     -hls_time 1 \
     -hls_list_size 20 \
     -hls_flags delete_segments \
     /live/stream.m3u8
-
-#  -ac 2 \
-    # -c:a aac \
-#  -c:a aac \
-    # -b:a 160000 \
-#   -f segment \
-    # -segment_time 2 \
-  #  -segment_list stream.m3u8 \
-#    "-vsync", "2", \
-  #"-v", "debug", \ 
-    # "-fflags", "nobuffer", \
-    # "-segment_format", "mpegts", \
-    # "-segment_list_flags", "+live", \
