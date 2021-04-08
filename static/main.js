00ec2b8a4968f6fcabc6205c97d0a7906ae80b55
Benjamin Rumble <therumbler@gmail.com>
2021-04-07 23:03:13 -0400

    changed hostname

--- static/main.js
+++ static/main.js
@@ -1,6 +1,6 @@
 (function () {
     const $ = document.querySelector.bind(document);
-    const BASE_URL = window.location.hostname == 'home.irumble.com'? '.' : 'https://home.irumble.com';
+    const BASE_URL = window.location.hostname == 'stream.irumble.com'? '.' : 'https://stream.irumble.com';
     async function fetchVideo(channel) {
         console.log(`fetchVideo: ${channel} `);
         let r = await fetch(`${BASE_URL}/api/streams/${channel}`);
